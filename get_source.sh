4ea361d43467c41b6d1c7b5d7a7b6b2f1aeeaa0c
Watal M. Iwasaki <heavy.watal@gmail.com>
2013-11-28 20:55:03 +0900

    moved Makefile.{am,in} to source directory

--- get_source.sh
+++ get_source.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 base_url=http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/SFMT/
 d_version=2.2.2
-if [ ! -e dSFMT-src-${d_version} ]; then
+if [ ! -e dSFMT-src-${d_version}/dSFMT.h ]; then
     curl -o - ${base_url}dSFMT-src-${d_version}.tar.gz | tar xz
 fi
-cp -u dSFMT-Makefile.am dSFMT-src-${d_version}/Makefile.am
 
 i_version=1.4
-if [ ! -e SFMT-src-${i_version} ]; then
+if [ ! -e SFMT-src-${i_version}/SFMT.h ]; then
     curl -o - ${base_url}SFMT-src-${i_version}.tar.gz | tar xz
 fi
-cp -u SFMT-Makefile.am SFMT-src-${i_version}/Makefile.am
