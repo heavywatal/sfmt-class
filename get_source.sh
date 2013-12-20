ef76de9646641a73f1f71cc2257b2f1bf315add2
Watal M. Iwasaki <heavy.watal@gmail.com>
2013-12-20 15:02:14 +0900

    followed official update of SFMT and dSFMT

--- get_source.sh
+++ get_source.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 base_url=http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/SFMT/
-d_version=2.2.2
+d_version=2.2.3
 if [ ! -e dSFMT-src-${d_version}/dSFMT.h ]; then
     curl -o - ${base_url}dSFMT-src-${d_version}.tar.gz | tar xz
 fi
 
-i_version=1.4
+i_version=1.4.1
 if [ ! -e SFMT-src-${i_version}/SFMT.h ]; then
     curl -o - ${base_url}SFMT-src-${i_version}.tar.gz | tar xz
 fi
