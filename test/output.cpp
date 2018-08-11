8777e7f049d1e9d9c928a9e8f019b3a34b37b6c8
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-08-12 01:02:15 +0900

    :white_check_mark: Increase flexibility of check_output()

--- test/output.cpp
+++ test/output.cpp
@@ -1,12 +1,12 @@
 #include <sfmt.hpp>
 
 #include <stdexcept>
+#include <sstream>
 #include <fstream>
 #include <string>
 
 template <class URBG> inline
-void check_output(const std::string path) {
-    URBG generator;
+void check_output(URBG& generator, const std::string path) {
     std::ifstream ifs(path);
     ifs.exceptions(std::ios_base::failbit | std::ios_base::badbit);
     std::string buffer;
@@ -16,14 +16,20 @@ void check_output(const std::string path) {
     uint64_t known_value;
     for (int i=0; i<1000; ++i) {
         ifs >> known_value;
-        if (generator() != known_value) {
-            throw std::runtime_error("Output is different from known value");
+        const auto x = generator();
+        if (x != known_value) {
+            std::ostringstream oss;
+            oss << "\nOutput is different from known value:\n"
+                << x << " != " << known_value;
+            throw std::runtime_error(oss.str());
         }
     }
 }
 
 int main() {
-    check_output<wtl::sfmt19937>("SFMT/SFMT.19937.out.txt");
-    check_output<wtl::sfmt19937_64>("SFMT/SFMT.19937.64.out.txt");
+    wtl::sfmt19937 engine32;
+    check_output(engine32, "SFMT/SFMT.19937.out.txt");
+    wtl::sfmt19937_64 engine64;
+    check_output(engine64, "SFMT/SFMT.19937.64.out.txt");
     return 0;
 }
