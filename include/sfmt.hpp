7953e904ae3aa41b980f1b50463776257441e45c
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-10-29 00:30:56 +0900

    :art: Use template class sfmt19937_engine<T>

--- include/sfmt.hpp
+++ include/sfmt.hpp
@@ -4,75 +4,43 @@
 
 #include <random>
 #include <limits>
+#include <type_traits>
 
 #include "SFMT/SFMT.h"
 
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 namespace wtl {
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 
-class sfmt19937 {
+template <class UIntType>
+class sfmt19937_engine {
   public:
-    using result_type = uint32_t;
+    using result_type = UIntType;
     using state_type = sfmt_t;
 
     static constexpr result_type min() {return 0U;}
     static constexpr result_type max() {return std::numeric_limits<result_type>::max();}
-    static constexpr result_type default_seed = 1234U;
+    static constexpr result_type default_seed = std::is_same_v<result_type, uint32_t> ? 1234U : 4321U;
 
-    // constructors
-    explicit sfmt19937(result_type s=default_seed) {seed(s);}
-    explicit sfmt19937(const state_type& state): state_(state) {}
-    sfmt19937(const sfmt19937&) = default;
-    sfmt19937(sfmt19937&&) = default;
+    sfmt19937_engine(): sfmt19937_engine(static_cast<uint32_t>(default_seed)) {}
+    explicit sfmt19937_engine(uint32_t s) {seed(s);}
+    explicit sfmt19937_engine(uint64_t s) {seed(s);}
+    explicit sfmt19937_engine(const state_type& state): state_(state) {}
+    sfmt19937_engine(const sfmt19937_engine&) = default;
+    sfmt19937_engine(sfmt19937_engine&&) = default;
 
-    // [0, 2^32-1]
     result_type operator()() {
+      if constexpr (std::is_same_v<result_type, uint32_t>) {
         return sfmt_genrand_uint32(&state_);
-    }
-
-    void seed(result_type s) {
-        sfmt_init_gen_rand(&state_, s);
-    }
-
-    void discard(unsigned long long n) {
-        for (; n != 0ULL; --n) {(*this)();}
-    }
-
-    const state_type& getstate() const {return state_;}
-    void setstate(const state_type& state) {state_ = state;}
-
-  private:
-    state_type state_;
-};
-
-
-class sfmt19937_64 {
-  public:
-    using result_type = uint64_t;
-    using state_type = sfmt_t;
-
-    static constexpr result_type min() {return 0U;}
-    static constexpr result_type max() {return std::numeric_limits<result_type>::max();}
-    static constexpr result_type default_seed = 4321U;
-
-    // constructors
-    explicit sfmt19937_64(uint32_t s=static_cast<uint32_t>(default_seed)) {seed(s);}
-    explicit sfmt19937_64(result_type s) {seed(s);}
-    explicit sfmt19937_64(const state_type& state): state_(state) {}
-    sfmt19937_64(const sfmt19937_64&) = default;
-    sfmt19937_64(sfmt19937_64&&) = default;
-
-    // [0, 2^64-1]
-    result_type operator()() {
+      } else {
         return sfmt_genrand_uint64(&state_);
+      }
     }
 
     void seed(uint32_t s) {
         sfmt_init_gen_rand(&state_, s);
     }
 
-    void seed(result_type s) {
+    void seed(uint64_t s) {
         sfmt_init_by_array(&state_, reinterpret_cast<uint32_t*>(&s), 2);
     }
 
@@ -87,7 +55,8 @@ class sfmt19937_64 {
     state_type state_;
 };
 
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
+using sfmt19937 = sfmt19937_engine<uint32_t>;
+using sfmt19937_64 = sfmt19937_engine<uint64_t>;
 
 inline sfmt19937& sfmt() {
     static sfmt19937 generator(std::random_device{}());
@@ -99,8 +68,7 @@ inline sfmt19937_64& sfmt64() {
     return generator;
 }
 
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 } // namespace wtl
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 
-#endif /* WTL_SFMT_HPP_ */
+#endif // WTL_SFMT_HPP_
